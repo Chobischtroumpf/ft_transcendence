d9f14e0d63d9f4cc894bea393d141b619e192e07
IgorJacquet <fcebook7889@gmail.com>
2022-07-27 22:03:58 +0200

    error404

--- frontend/src/pages/errors/Error404.tsx
+++ frontend/src/pages/errors/Error404.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Wrapper from "../../components/Wrapper";
+import errorPng from "../../assets/Error404.png"
 
 const Error404 = () => (
 	<Wrapper>
   <div>
-    <h1>404 - Not Found!</h1>
+    <img src={errorPng}/>
     <Link to="/">Go Home</Link>
   </div></Wrapper>
 );
