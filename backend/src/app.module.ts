9840ff5482247159ef8d43f21d1317c976558a4c
Mikko <mikkonuba123@gmail.com>
2022-08-09 16:14:26 +0300

    kk

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,7 +13,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs/`);
 
 @Module({
   imports: [
-    ConfigModule.forRoot({envFilePath, isGlobal: true }),
+    ConfigModule.forRoot({envFilePath: '../.env', isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
